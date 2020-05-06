d195d1f2d615bd1257fdf384b03068256801d4d4
Dmitriy Borisenko <otselnik@yandex.ru>
2020-05-06 10:58:16 +0400

    move metalog to own namespace

--- metalibs/meta_log/include/meta_log.hpp
+++ metalibs/meta_log/include/meta_log.hpp
@@ -4,35 +4,36 @@
 #define DEBUG_OUTPUT
 
 #ifdef DEBUG_OUTPUT
+
 #include <concurrentqueue.h>
 #include <sstream>
 #include <thread>
 
-extern moodycamel::ConcurrentQueue<std::stringstream*>* __output_queue;
-extern std::thread* __cout_printer;
-
-void print_to_stream_date_and_place(std::stringstream * p_ss, const std::string& file, const std::string& function, int line);
-
 #endif
 
-#define DEBUG_COUT(message) _debug_cout(true, std::string(__FILE__), std::string(&__func__[0]), __LINE__, message)
-#define DEBUG_COUT_COND(print, message) _debug_cout(print, std::string(__FILE__), std::string(&__func__[0]), __LINE__, message)
+#define DEBUG_COUT(message) meta_log::debug_cout(true, std::string(__FILE__), std::string(&__func__[0]), __LINE__, message)
+#define DEBUG_COUT_COND(print, message) meta_log::debug_cout(print, std::string(__FILE__), std::string(&__func__[0]), __LINE__, message)
 
-template <typename Message>
-inline void _debug_cout(bool print, const std::string& file, const std::string& function, const int line, const Message& msg)
-{
-    if (print) {
+namespace meta_log {
+    extern moodycamel::ConcurrentQueue<std::stringstream *> *output_queue;
+    extern std::thread *cout_printer;
+
+    void print_to_stream_date_and_place(std::stringstream *p_ss, const std::string &file, const std::string &function, int line);
+
+    template<typename Message>
+    inline void
+    debug_cout(bool print, const std::string &file, const std::string &function, const int line, const Message &msg) {
+        if (print) {
 #ifdef DEBUG_OUTPUT
 
-        auto* p_ssout = new std::stringstream;
-
-        print_to_stream_date_and_place(p_ssout, file, function, line);
-
-        (*p_ssout) << msg;
-
-        __output_queue->enqueue(p_ssout);
+            auto *p_ssout = new std::stringstream;
+            print_to_stream_date_and_place(p_ssout, file, function, line);
+            (*p_ssout) << msg;
+            __output_queue->enqueue(p_ssout);
 #endif
+        }
     }
 }
+
 
 #endif // META_LOG_HPP
--- metalibs/meta_log/src/meta_log.cpp
+++ metalibs/meta_log/src/meta_log.cpp
@@ -7,41 +7,42 @@
 
 #include "date.h"
 
-moodycamel::ConcurrentQueue<std::stringstream*>* __output_queue = new moodycamel::ConcurrentQueue<std::stringstream*>();
-std::thread* __cout_printer = new std::thread([]() {
-    moodycamel::ConsumerToken ct(*__output_queue);
+namespace meta_log {
+    auto output_queue = new moodycamel::ConcurrentQueue<std::stringstream *>();
+    auto cout_printer = new std::thread([]() {
+        moodycamel::ConsumerToken ct(*output_queue);
 
-    bool do_forever = true;
-    while (do_forever) {
-        std::stringstream* p_ssout;
-        if (__output_queue->try_dequeue(ct, p_ssout)) {
+        for (;;) {
+            std::stringstream *p_ssout;
+            if (output_queue->try_dequeue(ct, p_ssout)) {
 
-            std::cout << p_ssout->rdbuf() << std::endl;
+                std::cout << p_ssout->rdbuf() << std::endl;
 
-            delete p_ssout;
-        } else {
-            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+                delete p_ssout;
+            } else {
+                std::this_thread::sleep_for(std::chrono::milliseconds(1));
+            }
         }
+    });
+
+    void print_to_stream_date_and_place(std::stringstream *p_ss, const std::string &file, const std::string &function,
+                                        const int line) {
+        const auto found = file.find_last_of("/\\");
+        const auto filename = (found == std::string::npos) ? file : file.substr(found + 1);
+
+        auto tp = std::chrono::system_clock::now();
+        auto dp = date::floor<date::days>(tp);
+
+        auto ymd = date::year_month_day{dp};
+        auto time = date::make_time(std::chrono::duration_cast<std::chrono::milliseconds>(tp - dp));
+
+        uint64_t micro = std::chrono::duration_cast<std::chrono::microseconds>(tp - dp).count() % 1000000l;
+
+        (*p_ss) << ymd.year() << "/" << ymd.month() << "/" << ymd.day() << " "
+                << std::setfill(' ') << std::setw(2) << time.hours().count() << ":"
+                << std::setfill('0') << std::setw(2) << time.minutes().count() << ":"
+                << std::setfill('0') << std::setw(2) << time.seconds().count() << ":"
+                << std::setfill('0') << std::setw(6) << micro << " "
+                << filename << ":" << line << ":" << function << "$\t";
     }
-});
-
-void print_to_stream_date_and_place(std::stringstream* p_ss, const std::string& file, const std::string& function, const int line)
-{
-    const auto found = file.find_last_of("/\\");
-    const auto filename = (found == std::string::npos) ? file : file.substr(found + 1);
-
-    auto tp = std::chrono::system_clock::now();
-    auto dp = date::floor<date::days>(tp);
-
-    auto ymd = date::year_month_day{ dp };
-    auto time = date::make_time(std::chrono::duration_cast<std::chrono::milliseconds>(tp - dp));
-
-    uint64_t micro = std::chrono::duration_cast<std::chrono::microseconds>(tp - dp).count() % 1000000l;
-
-    (*p_ss) << ymd.year() << "/" << ymd.month() << "/" << ymd.day() << " "
-            << std::setfill(' ') << std::setw(2) << time.hours().count() << ":"
-            << std::setfill('0') << std::setw(2) << time.minutes().count() << ":"
-            << std::setfill('0') << std::setw(2) << time.seconds().count() << ":"
-            << std::setfill('0') << std::setw(6) << micro << " "
-            << filename << ":" << line << ":" << function << "$\t";
 }
