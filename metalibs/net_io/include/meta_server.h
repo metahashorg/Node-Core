c6e5d2d9821a018d98d33f6ed4648dc91e1c1a74
Dmitriy Borisenko <otselnik@yandex.ru>
2020-05-14 16:19:47 +0400

    network: meta server progress

--- metalibs/net_io/include/meta_server.h
+++ metalibs/net_io/include/meta_server.h
@@ -1,11 +1,17 @@
 #ifndef METANET_META_SERVER_H
 #define METANET_META_SERVER_H
 
-#include <unordered_map>
-
 #include <boost/array.hpp>
 #include <boost/asio.hpp>
 #include <boost/enable_shared_from_this.hpp>
+#include <functional>
+#include <unordered_set>
+
+#include <open_ssl_decor.h>
+
+namespace metahash::net_io {
+
+uint32_t METAHASH_MAGIC_NUMBER = 0x01abcdef;
 
 class request {
 public:
@@ -18,7 +24,7 @@ public:
     std::string sender_addr;
     std::string remote_address;
 
-    int8_t parse(char*, size_t);
+    int8_t parse(char*, size_t, std::unordered_set<std::string, crypto::DataHasher>& allowed_addreses);
 
 private:
     std::vector<char> request_full;
@@ -29,7 +35,7 @@ private:
     uint64_t sign_size = 0;
     uint64_t message_size = 0;
 
-    bool read_varint(uint64_t &varint);
+    bool read_varint(uint64_t& varint);
     bool fill_sw(std::string_view& sw, uint64_t sw_size);
 };
 
@@ -38,48 +44,59 @@ public:
     uint64_t reply_id = 0;
     std::vector<char> message;
 
-    std::vector<boost::asio::const_buffer> to_buffers();
-    void make(signer);
-
+    std::vector<boost::asio::const_buffer> make(crypto::Signer&);
 private:
 };
 
 class connection : public boost::enable_shared_from_this<connection> {
 public:
-    explicit connection(boost::asio::io_context& io_context);
+    explicit connection(boost::asio::io_context& io_context, std::function<void(Request&, Reply&)>& handler, crypto::Signer& signer, std::unordered_set<std::string, crypto::DataHasher>& allowed_addreses);
 
     boost::asio::ip::tcp::socket& get_socket();
 
     void start();
 
 private:
-    void handle_read(const boost::system::error_code& e,        std::size_t bytes_transferred);
-
+    void handle_read(const boost::system::error_code& e, std::size_t bytes_transferred);
     void handle_write(const boost::system::error_code& e);
+    void handle_write_and_close(const boost::system::error_code& e);
 
     boost::asio::strand<boost::asio::io_context::executor_type> strand;
 
     boost::asio::ip::tcp::socket socket;
 
     boost::array<char, 0xffff> buffer;
-
     request request;
+    reply reply;
 
+    std::function<void(Request&, Reply&)>& request_handler;
+    crypto::Signer& signer;
+    std::unordered_set<std::string, crypto::DataHasher>& allowed_addreses;
 };
 
 class meta_server {
 public:
-    explicit meta_server(boost::asio::io_context& io_context, const std::string& address, const std::string& port, request_handler);
+    meta_server(boost::asio::io_context& io_context,
+        const std::string& address,
+        const std::string& port,
+        std::function<void(Request&, Reply&)> request_handler,
+        crypto::Signer& signer,
+        std::unordered_set<std::string, crypto::DataHasher> allowed_addreses);
 
 private:
     void start_accept();
-
     void handle_accept(const boost::system::error_code& e);
 
     boost::asio::io_context& io_context;
     boost::asio::ip::tcp::acceptor acceptor;
 
+    std::function<void(Request&, Reply&)> request_handler;
+    crypto::Signer& signer;
+    std::unordered_set<std::string, crypto::DataHasher> allowed_addreses;
+
     boost::shared_ptr<connection> new_connection;
 };
+
+}
 
 #endif //METANET_META_SERVER_H
--- metalibs/net_io/src/meta_server.cpp
+++ metalibs/net_io/src/meta_server.cpp
@@ -1,14 +1,66 @@
 #include <boost/bind.hpp>
 #include <boost/lexical_cast.hpp>
 #include <boost/shared_ptr.hpp>
-#include <meta_server.h>
-#include <open_ssl_decor.h>
 
-meta_server::meta_server(boost::asio::io_context& io_context, const std::string& address, const std::string& port)
+#include <meta_server.h>
+#include <version.h>
+
+namespace metahash::net_io {
+
+namespace statics {
+    enum parse_state {
+        SUCCESS,
+        INCOMPLETE,
+        WRONG_MAGIC_NUMBER,
+        UNKNOWN_SENDER_METAHASH_ADDRESS,
+        INVALID_SIGN,
+    };
+
+    std::vector<char> version_info(const std::string& mh_addr)
+    {
+        static const std::string version = std::string(VESION_MAJOR) + "." + std::string(VESION_MINOR) + "." + std::string(GIT_COUNT);
+        std::string resp_data = "{\"result\":{\"version\":\"" + version + "\",\"mh_addr\": \"" + mh_addr + "\"}}";
+        static const std::string http_head = "HTTP/1.1 200 OK\r\n"
+                                             "Version: HTTP/1.1\r\n"
+                                             "Content-Type: text/json; charset=utf-8\r\n"
+                                             "Content-Length: ";
+        static const std::string rnrn = "\r\n\r\n";
+
+        std::vector<char> write_buff;
+        write_buff.insert(write_buff.end(), http_head.begin(), http_head.end());
+        std::string data_size_str = std::to_string(resp_data.size());
+        write_buff.insert(write_buff.end(), data_size_str.begin(), data_size_str.end());
+        write_buff.insert(write_buff.end(), rnrn.begin(), rnrn.end());
+        write_buff.insert(write_buff.end(), resp_data.begin(), resp_data.end());
+    }
+
+    std::vector<char> unkown_sender()
+    {
+        std::vector<char> write_buff;
+
+        return write_buff;
+    }
+
+    std::vector<char> invalid_sign()
+    {
+        std::vector<char> write_buff;
+
+        return write_buff;
+    }
+}
+
+meta_server::meta_server(boost::asio::io_context& io_context,
+    const std::string& address,
+    const std::string& port,
+    std::function<void(Request&, Reply&)> request_handler,
+    crypto::Signer& signer,
+    std::unordered_set<std::string, crypto::DataHasher> allowed_addreses)
     : io_context(io_context)
     , acceptor(io_context)
+    , request_handler(request_handler)
+    , signer(signer)
+    , allowed_addreses(allowed_addreses)
     , new_connection()
-    , request_handler()
 {
     boost::asio::ip::tcp::resolver resolver(io_context);
     boost::asio::ip::tcp::endpoint endpoint = *resolver.resolve(address, port).begin();
@@ -22,7 +74,7 @@ meta_server::meta_server(boost::asio::io_context& io_context, const std::string&
 
 void meta_server::start_accept()
 {
-    new_connection.reset(new connection(io_context, request_handler));
+    new_connection.reset(new connection(io_context, request_handler, signer, allowed_addreses));
     acceptor.async_accept(new_connection->get_socket(), boost::bind(&meta_server::handle_accept, this, boost::asio::placeholders::error));
 }
 
@@ -35,10 +87,12 @@ void meta_server::handle_accept(const boost::system::error_code& e)
     start_accept();
 }
 
-connection::connection(boost::asio::io_context& io_context, request_handler& handler)
-    : strand(boost::asio::make_strand(io_context))
+connection::connection(boost::asio::io_context& io_context, std::function<void(Request&, Reply&)>& handler, crypto::Signer& signer, std::unordered_set<std::string, crypto::DataHasher>& allowed_addreses)
+    : strand(boost::asio::make_strand(io_context.get_executor()))
     , socket(strand)
     , request_handler(handler)
+    , signer(signer)
+    , allowed_addreses(allowed_addreses)
 {
 }
 
@@ -55,20 +109,33 @@ void connection::start()
 void connection::handle_read(const boost::system::error_code& e, std::size_t bytes_transferred)
 {
     if (!e) {
-        int8_t result = request.parse(buffer.data(), bytes_transferred);
+        auto result = static_cast<statics::parse_state>(request.parse(buffer.data(), bytes_transferred));
 
         switch (result) {
-        case 1: {
+        case statics::SUCCESS: {
             request.remote_address = socket.remote_endpoint().address().to_string();
-            std::vector<char> reply = request_handler(request);
-            boost::asio::async_write(socket, reply, boost::bind(&connection::handle_write, shared_from_this(), boost::asio::placeholders::error));
+            reply.reply_id = request.request_id;
+
+            request_handler(request, reply);
+
+            boost::asio::async_write(socket, reply.make(signer), boost::bind(&connection::handle_write, shared_from_this(), boost::asio::placeholders::error));
         } break;
-        case 0: {
+        case statics::INCOMPLETE: {
             socket.async_read_some(boost::asio::buffer(buffer), boost::bind(&connection::handle_read, shared_from_this(), boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred));
         } break;
+        case statics::WRONG_MAGIC_NUMBER: {
+            boost::asio::async_write(socket, boost::asio::buffer(statics::version_info(signer.get_mh_addr())), boost::bind(&connection::handle_write_and_close, shared_from_this(), boost::asio::placeholders::error));
+        } break;
+        case statics::UNKNOWN_SENDER_METAHASH_ADDRESS: {
+            reply.message = statics::unkown_sender();
+            boost::asio::async_write(socket, reply.make(signer), boost::bind(&connection::handle_write_and_close, shared_from_this(), boost::asio::placeholders::error));
+        } break;
+        case statics::INVALID_SIGN: {
+            reply.message = statics::invalid_sign();
+            boost::asio::async_write(socket, reply.make(signer), boost::bind(&connection::handle_write_and_close, shared_from_this(), boost::asio::placeholders::error));
+        } break;
         default: {
-            reply = reply::stock_reply(reply::bad_request);
-            boost::asio::async_write(socket, version_info, boost::bind(&connection::handle_write, shared_from_this(), boost::asio::placeholders::error));
+            boost::asio::async_write(socket, boost::asio::buffer(statics::version_info(signer.get_mh_addr())), boost::bind(&connection::handle_write_and_close, shared_from_this(), boost::asio::placeholders::error));
         }
         }
     }
@@ -81,17 +148,38 @@ void connection::handle_write(const boost::system::error_code& e)
     }
 }
 
-std::vector<boost::asio::const_buffer> reply::to_buffers()
+void connection::handle_write_and_close(const boost::system::error_code& e)
 {
-    std::vector<boost::asio::const_buffer> buffers;
-
-    return buffers;
+    if (!e) {
+        boost::system::error_code ignored_ec;
+        socket.shutdown(boost::asio::ip::tcp::socket::shutdown_both, ignored_ec);
+    }
 }
 
-bool request::read_varint(uint64_t &varint)
+std::vector<boost::asio::const_buffer> reply::make(crypto::Signer& signer)
+{
+    std::vector<char> write_buff;
+
+    std::vector<char> public_key = signer.get_pub_key();
+    std::vector<char> sign = signer.sign(message);
+
+    write_buff.insert(write_buff.end(), reinterpret_cast<char*>(&METAHASH_MAGIC_NUMBER), (reinterpret_cast<char*>(&METAHASH_MAGIC_NUMBER) + sizeof(uint32_t)));
+    crypto::append_varint(write_buff, reply_id);
+
+    crypto::append_varint(write_buff, public_key.size());
+    write_buff.insert(write_buff.end(),public_key.begin(), public_key.end());
+
+    crypto::append_varint(write_buff, sign.size());
+    write_buff.insert(write_buff.end(),sign.begin(), sign.end());
+
+    crypto::append_varint(write_buff, message.size());
+    write_buff.insert(write_buff.end(),message.begin(), message.end());
+}
+
+bool request::read_varint(uint64_t& varint)
 {
     auto previous_offset = offset;
-    offset += ::read_varint(varint, std::string_view(&request_full[offset], request_full.size() - offset));
+    offset += crypto::read_varint(varint, std::string_view(&request_full[offset], request_full.size() - offset));
     return offset != previous_offset;
 }
 
@@ -106,7 +194,7 @@ bool request::fill_sw(std::string_view& sw, uint64_t sw_size)
     }
 }
 
-int8_t request::parse(char* buff_data, size_t buff_size)
+int8_t request::parse(char* buff_data, size_t buff_size, std::unordered_set<std::string, crypto::DataHasher>& allowed_addreses)
 {
     request_full.insert(request_full.end(), buff_data, buff_data + buff_size);
 
@@ -114,61 +202,62 @@ int8_t request::parse(char* buff_data, size_t buff_size)
         if (request_full.size() >= sizeof(uint32_t)) {
             magic_number = *(reinterpret_cast<uint32_t*>(&request_full[0]));
             if (magic_number != METAHASH_MAGIC_NUMBER) {
-                return WRONG_MAGIC_NUMBER;
+                return statics::WRONG_MAGIC_NUMBER;
             }
             offset = sizeof(uint32_t);
-            current_state = 10;
         } else {
-            return 0;
+            return statics::INCOMPLETE;
         }
     }
 
     if (request_id == 0 && !read_varint(request_id)) {
-        return 0;
+        return statics::INCOMPLETE;
     }
 
     if (request_type == 0 && !read_varint(request_type)) {
-        return 0;
+        return statics::INCOMPLETE;
     }
 
     if (public_key_size == 0 && !read_varint(public_key_size)) {
-        return 0;
+        return statics::INCOMPLETE;
     }
 
     if (public_key.empty()) {
         if (fill_sw(public_key, public_key_size)) {
-            sender_addr = "0x" + bin2hex(get_address(public_key));
+            sender_addr = "0x" + crypto::bin2hex(crypto::get_address(public_key));
             if (!allowed_addreses.empty()) {
-                if (allowed_addreses.find(get_address(public_key)) == allowed_addreses.end()) {
-                    return UNKNOWN_SENDER_METAHASH_ADDRESS;
+                if (allowed_addreses.find(sender_addr) == allowed_addreses.end()) {
+                    return statics::UNKNOWN_SENDER_METAHASH_ADDRESS;
                 }
             }
         } else {
-            return 0;
+            return statics::INCOMPLETE;
         }
     }
 
     if (sign_size == 0 && !read_varint(sign_size)) {
-        return 0;
+        return statics::INCOMPLETE;
     }
 
     if (sign.empty() && fill_sw(sign, sign_size)) {
-        return 0;
+        return statics::INCOMPLETE;
     }
 
     if (message_size == 0 && !read_varint(message_size)) {
-        return 0;
+        return statics::INCOMPLETE;
     }
 
     if (message.empty()) {
         if (fill_sw(message, message_size)) {
-            if (!check_sign(message, sign, public_key)) {
-                return INVALID_SIGN;
+            if (!crypto::check_sign(message, sign, public_key)) {
+                return statics::INVALID_SIGN;
             }
         } else {
-            return 0;
+            return statics::INCOMPLETE;
         }
     }
 
-    return 1;
+    return statics::SUCCESS;
+}
+
 }
