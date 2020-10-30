bf0452145554470a386699b244b1578f892f3051
Dmitriy Borisenko <otselnik@yandex.ru>
2020-10-30 11:17:33 +0400

    Update meta_constants.hpp
    set timouts from debug to release values

--- metalibs/meta_constants/meta_constants.hpp
+++ metalibs/meta_constants/meta_constants.hpp
@@ -118,9 +118,9 @@ const uint64_t TX_REJECT_INSUFFICIENT_FUNDS_EXT = 0xff05;
 const uint64_t TX_REJECT_FOUNDER_LIMIT = 0xff06;
 const uint64_t TX_REJECT_INVALID_WALLET = 0x0404;
 
-const uint64_t DAY_IN_SECONDS = 5 * 60; //24 * 60 * 60;
+const uint64_t DAY_IN_SECONDS = 24 * 60 * 60;
 const uint64_t CORE_LIST_RENEW_PERIOD = 10 * 60;
-const uint64_t CORE_LIST_SILENCE_PERIOD = 15;
+const uint64_t CORE_LIST_SILENCE_PERIOD = 30;
 
 /*                          RPC METHODS                           */
 const uint64_t RPC_TX = 0x20;
