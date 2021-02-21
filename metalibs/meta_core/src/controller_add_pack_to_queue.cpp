3a19cb5c375438dd160ddf585115ddeb22cb3623
Dmitriy Borisenko <otselnik@yandex.ru>
2021-02-21 22:39:57 +0300

    meta_core: clearer minute log

--- metalibs/meta_core/src/controller_add_pack_to_queue.cpp
+++ metalibs/meta_core/src/controller_add_pack_to_queue.cpp
@@ -134,85 +134,90 @@ void ControllerImplementation::log_network_statistics(uint64_t timestamp)
 {
     /// DEBUG INFO BEGIN
     if (timestamp > dbg_timestamp + 60) {
-        DEBUG_COUT("0x00112233445566778899aabbccddeeffgghhffjjiikkllmmnn\tTX\tGetCL\tApprove\tDisAprv\tGetAprv\tAprvLst\tLastBlo\tGetBlok\tGetChai\tGetBLst\tCoreLst\tPretend\tNone\tRoles\tIP");
+        std::string log_msg = "\n";
+        log_msg += "0x00112233445566778899aabbccddeeffgghhffjjiikkllmmnn\tTX\tGetCL\tApprove\tDisAprv\tGetAprv\tAprvLst\tLastBlo\tGetBlok\tGetChai\tGetBLst\tCoreLst\tPretend\tNone\tRoles\tIP";
 
-        std::shared_lock lock(income_nodes_stat_lock);
-        for (auto&& node_stat : income_nodes_stat) {
-            auto&& stat = node_stat.second;
-            auto&& addr = node_stat.first;
+        {
+            std::shared_lock lock(income_nodes_stat_lock);
+            for (auto&& node_stat : income_nodes_stat) {
+                auto&& stat = node_stat.second;
+                auto&& addr = node_stat.first;
 
-            std::string role_list;
-            {
-                auto roles = BC.check_addr(addr);
-                for (const auto& role : roles) {
-                    role_list += role + "\t";
+                std::string role_list;
+                {
+                    auto roles = BC.check_addr(addr);
+                    for (const auto& role : roles) {
+                        role_list += role + "\t";
+                    }
                 }
-            }
 
-            std::string ip_list;
-            {
-                std::shared_lock lock(stat.ip_lock);
-                for (const auto& ip : stat.ip) {
-                    ip_list += ip + "\t";
+                std::string ip_list;
+                {
+                    std::shared_lock lock(stat.ip_lock);
+                    for (const auto& ip : stat.ip) {
+                        ip_list += ip + "\t";
+                    }
                 }
-            }
 
-            auto total_stat = stat.dbg_RPC_TX
-                + stat.dbg_RPC_GET_CORE_LIST
-                + stat.dbg_RPC_APPROVE
-                + stat.dbg_RPC_DISAPPROVE
-                + stat.dbg_RPC_GET_APPROVE
-                + stat.dbg_RPC_APPROVE_LIST
-                + stat.dbg_RPC_LAST_BLOCK
-                + stat.dbg_RPC_GET_BLOCK
-                + stat.dbg_RPC_GET_CHAIN
-                + stat.dbg_RPC_GET_MISSING_BLOCK_LIST
-                + stat.dbg_RPC_CORE_LIST_APPROVE
-                + stat.dbg_RPC_PRETEND_BLOCK
-                + stat.dbg_RPC_NONE;
+                auto total_stat = stat.dbg_RPC_TX
+                    + stat.dbg_RPC_GET_CORE_LIST
+                    + stat.dbg_RPC_APPROVE
+                    + stat.dbg_RPC_DISAPPROVE
+                    + stat.dbg_RPC_GET_APPROVE
+                    + stat.dbg_RPC_APPROVE_LIST
+                    + stat.dbg_RPC_LAST_BLOCK
+                    + stat.dbg_RPC_GET_BLOCK
+                    + stat.dbg_RPC_GET_CHAIN
+                    + stat.dbg_RPC_GET_MISSING_BLOCK_LIST
+                    + stat.dbg_RPC_CORE_LIST_APPROVE
+                    + stat.dbg_RPC_PRETEND_BLOCK
+                    + stat.dbg_RPC_NONE;
 
-            if (total_stat == 0 && ip_list.empty()) {
-                continue;
-            }
+                if (total_stat == 0 && ip_list.empty()) {
+                    continue;
+                }
 
-            DEBUG_COUT(addr + "\t"
-                + std::to_string(stat.dbg_RPC_TX) + "\t"
-                + std::to_string(stat.dbg_RPC_GET_CORE_LIST) + "\t"
-                + std::to_string(stat.dbg_RPC_APPROVE) + "\t"
-                + std::to_string(stat.dbg_RPC_DISAPPROVE) + "\t"
-                + std::to_string(stat.dbg_RPC_GET_APPROVE) + "\t"
-                + std::to_string(stat.dbg_RPC_APPROVE_LIST) + "\t"
-                + std::to_string(stat.dbg_RPC_LAST_BLOCK) + "\t"
-                + std::to_string(stat.dbg_RPC_GET_BLOCK) + "\t"
-                + std::to_string(stat.dbg_RPC_GET_CHAIN) + "\t"
-                + std::to_string(stat.dbg_RPC_GET_MISSING_BLOCK_LIST) + "\t"
-                + std::to_string(stat.dbg_RPC_CORE_LIST_APPROVE) + "\t"
-                + std::to_string(stat.dbg_RPC_PRETEND_BLOCK) + "\t"
-                + std::to_string(stat.dbg_RPC_NONE) + "\t"
-                + role_list
-                + ip_list);
+                log_msg += addr + "\t"
+                    + std::to_string(stat.dbg_RPC_TX) + "\t"
+                    + std::to_string(stat.dbg_RPC_GET_CORE_LIST) + "\t"
+                    + std::to_string(stat.dbg_RPC_APPROVE) + "\t"
+                    + std::to_string(stat.dbg_RPC_DISAPPROVE) + "\t"
+                    + std::to_string(stat.dbg_RPC_GET_APPROVE) + "\t"
+                    + std::to_string(stat.dbg_RPC_APPROVE_LIST) + "\t"
+                    + std::to_string(stat.dbg_RPC_LAST_BLOCK) + "\t"
+                    + std::to_string(stat.dbg_RPC_GET_BLOCK) + "\t"
+                    + std::to_string(stat.dbg_RPC_GET_CHAIN) + "\t"
+                    + std::to_string(stat.dbg_RPC_GET_MISSING_BLOCK_LIST) + "\t"
+                    + std::to_string(stat.dbg_RPC_CORE_LIST_APPROVE) + "\t"
+                    + std::to_string(stat.dbg_RPC_PRETEND_BLOCK) + "\t"
+                    + std::to_string(stat.dbg_RPC_NONE) + "\t"
+                    + role_list
+                    + ip_list;
 
-            stat.dbg_RPC_TX = 0;
-            stat.dbg_RPC_GET_CORE_LIST = 0;
-            stat.dbg_RPC_APPROVE = 0;
-            stat.dbg_RPC_DISAPPROVE = 0;
-            stat.dbg_RPC_GET_APPROVE = 0;
-            stat.dbg_RPC_APPROVE_LIST = 0;
-            stat.dbg_RPC_LAST_BLOCK = 0;
-            stat.dbg_RPC_GET_BLOCK = 0;
-            stat.dbg_RPC_GET_CHAIN = 0;
-            stat.dbg_RPC_GET_MISSING_BLOCK_LIST = 0;
-            stat.dbg_RPC_CORE_LIST_APPROVE = 0;
-            stat.dbg_RPC_PRETEND_BLOCK = 0;
-            stat.dbg_RPC_NONE = 0;
+                stat.dbg_RPC_TX = 0;
+                stat.dbg_RPC_GET_CORE_LIST = 0;
+                stat.dbg_RPC_APPROVE = 0;
+                stat.dbg_RPC_DISAPPROVE = 0;
+                stat.dbg_RPC_GET_APPROVE = 0;
+                stat.dbg_RPC_APPROVE_LIST = 0;
+                stat.dbg_RPC_LAST_BLOCK = 0;
+                stat.dbg_RPC_GET_BLOCK = 0;
+                stat.dbg_RPC_GET_CHAIN = 0;
+                stat.dbg_RPC_GET_MISSING_BLOCK_LIST = 0;
+                stat.dbg_RPC_CORE_LIST_APPROVE = 0;
+                stat.dbg_RPC_PRETEND_BLOCK = 0;
+                stat.dbg_RPC_NONE = 0;
 
-            {
-                std::unique_lock lock(stat.ip_lock);
-                stat.ip.clear();
+                {
+                    std::unique_lock lock(stat.ip_lock);
+                    stat.ip.clear();
+                }
             }
         }
 
         dbg_timestamp = timestamp;
+
+        DEBUG_COUT(log_msg);
     }
     /// DEBUG INFO END
 }
